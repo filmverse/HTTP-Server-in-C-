a93dacc9502856b3ad6c10ca5b46360966ac539e
VIKAS SHARMA <vkassharma19492@gmail.com>
2025-02-04 00:57:13 +0530

    using string

--- main.cpp
+++ main.cpp
@@ -1,10 +1,19 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main(){
-    int number = 0;
-    cin >> number;
-    printf("Your ID is %d\n", number + 3);
+    string myColor;
+    string userFirstName;
+    string userLastName;
+
+    cout << "Enter your first name \n";
+    getline(cin, userFirstName);
+
+    cout << "Enter your last name \n";
+    getline(cin, userLastName);
+
+    cout << "User first name is " << userFirstName << " and last name is " << userLastName << ". Good." << endl;
     return 0;
 }
